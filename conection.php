205b825f6d8d32fc3607fbe3b47462aea23bc43f
fabianpid <fabianpid34@gmail.com>
2026-04-24 21:56:27 -0600

    Add files via upload

--- conection.php
+++ conection.php
@@ -9,11 +9,12 @@ error_reporting(E_ALL);
  if (isset($_POST['enviar'])) {
     $cliente =($_POST['cliente']);
     $postre =($_POST['postre']);
-    $numero_contacto =($_POST['correo']);
+    $correo =($_POST['correo']);
     $cantidad =($_POST['cantidad']);
     $mensaje =($_POST['mensaje']);
 
     $sql="INSERT INTO pedidos(cliente, postre, correo,cantidad,mensaje) VALUES ('$cliente', '$postre', '$correo','$cantidad','$mensaje')";
+$query = mysqli_query($con, $sql);
 
  if($query){
         echo "¡Éxito! Usuario insertado.";
